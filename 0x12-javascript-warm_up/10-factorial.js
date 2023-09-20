bb037cd1a748144e8d3ef5d063354edc1350faaa
Boolayinka <boolayinka@gmail.com>
2023-09-20 09:06:42 -0700

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+const numb = process.argv[2];
+
+function factorial (numb) {
+  if (numb <= 1 || isNaN(numb)) {
+    return 1;
+  } else {
+    return numb * factorial(numb - 1);
+  }
 }
 
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(numb));
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+const sqr = process.argv[2];
+if (isNaN(sqr)) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
-    let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
-    console.log(row);
+  for (let x = 0; x < sqr; x++) {
+    console.log('X'.repeat(sqr));
   }
 }
